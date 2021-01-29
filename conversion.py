4475f02fda1dbe5ac3cc9da1283e38a3a9647bd6
brauliorivas <brauliorivas2004@gmail.com>
2021-01-29 13:31:24 -0500

    Funciones

--- conversion.py
+++ conversion.py
@@ -1,3 +1,5 @@
+#python no es debilmente tipado, solo se pueden combinar los mismo tipos de datos en el print
+
 menu = """
 Bienvenido al conversor de monedas
 
@@ -9,14 +11,18 @@ Elige una opción: """
 
 opcion = int(input(menu))
 
+def conversion(tasa, pais):
+    pesos = input('¿Cuántos pesos ' + pais + ' tienes?:  ')
+    pesos = float(pesos)
+    valor_dolar = tasa
+    dolar = str(round( pesos / valor_dolar, 2))
+    print('Tienes $' + dolar + ' dolares en tu cuenta')
 
-
-
-dolares = input('¿Cuántos dolares tienes?:  ')
-dolares = float(dolares)
-
-valor_yen = 104.473
-
-yenes = str(round( dolares * valor_yen, 2))
-#python no es debilmente tipado, solo se pueden combinar los mismo tipos de datos en el print
-print('Tienes $' + yenes + ' yenes japoneses en tu cuenta') 
+if opcion == 1:
+    conversion(3875, 'colombianos')
+elif opcion == 2:
+    conversion(65, 'argentinos')
+elif opcion == 3:
+    conversion(24, 'mexicanos')
+else:
+    print('Ingrese una respuesta válida')
